8ba683bc02b693c1ed587398e43d16fdc392e9b1
BenjaminBogBog <Benjaminbogbog@gmail.com>
2024-05-05 20:07:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/', function (req, res) {
-  res.redirect("https://docs.google.com/forms/d/1_dEp0dvqjeIlhLBuWKImmsP2XBglojH2X4oMl4R6aFI");
+  res.redirect("https://docs.google.com/forms/d/e/1FAIpQLSdj8ljtthbybtfUzAkVYswn7Jw_D2zbeN4hdbF9zh6H42Ro3A/viewform");
 })
 
 app.listen(3000)

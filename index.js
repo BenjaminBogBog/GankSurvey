0971119a009362464126ad45510b58a6d95f355e
BenjaminBogBog <Benjaminbogbog@gmail.com>
2024-05-05 23:32:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.redirect("https://docs.google.com/forms/d/e/1FAIpQLSdj8ljtthbybtfUzAkVYswn7Jw_D2zbeN4hdbF9zh6H42Ro3A/viewform");
 })
 
+// https://imgur.com/a/i6zDL8n
+
+app.get('/stat', function (req, res) {
+  res.redirect("https://imgur.com/a/i6zDL8n");
+})
+
 app.listen(3000)
